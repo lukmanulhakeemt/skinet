81edbec092bf1e5372f5598e1234aed3e4ae4cf6
Lukmanul Hakeem <tlukmanulhakeem@gmail.com>
2021-09-07 20:30:50 +0530

    API Validation- Section 16

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -65,7 +65,7 @@ namespace API.Controllers
         var user = await _userManager.FindByUserByClaimsPrincipleWithAddressAsync(User);
         user.Address = _mapper.Map<AddressDto,Address>(address);
         var result = await _userManager.UpdateAsync(user);
-        
+
         if(result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
 
         return BadRequest("Problem Updating the USer");
@@ -93,6 +93,14 @@ namespace API.Controllers
     [HttpPost("register")]
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
+        if(CheckEmailExistsAsync(registerDto.Email).Result.Value)
+        {
+            return new BadRequestObjectResult(new ApiValidationErrorReponse
+            {
+                Errors=new[]{"Email address is in use"}
+            });
+        }
+
         var user = new AppUser
         {
             DisplayName = registerDto.DisplayName,
--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+        ErrorMessage ="Passwordshould contain at least 1 lower,upper,digit, special charmin6 max10 chars")]
         public string Password { get; set; }
     }
 }
